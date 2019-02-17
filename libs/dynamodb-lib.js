6ecb382507bc10a03d4e006cc5413001557e9f76
Alex <alex@wendte.tech>
2019-02-17 13:44:41 -0600

    adds eslint to dev-deps

--- libs/dynamodb-lib.js
+++
@@ -1,8 +0,0 @@
-import AWS from 'aws-sdk'
-
-// eslint-disable-next-line
-export function call(action, params) {
-  const dynamoDb = new AWS.DynamoDB.DocumentClient()
-
-  return dynamoDb[action](params).promise()
-}
